b5e609c941c9ced3a7c27177ef2c546b762c0fa4
Natalie Halawani <hala1224@gmail.com>
2019-02-17 04:13:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,12 +7,3 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
 (6,"Grinch", 7, "Male", "Brown", "Grumpy", 0),
 (7,"Wendy", 4, "Female", "Black", "Shy", 1),
 (8,null, 6, "Male", "Black", "Aggresive", 1);
--- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
--- (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
--- (2,"Rowdy", 10, "M", "black", "intense", 1),
--- (3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
--- (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
--- (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
--- (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
--- (7,"Wendy", 6, "F", "Blue", "naive", 1),
--- (8,null, 20, "M", "black", "aggressive", 0);
