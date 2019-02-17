ccbc27d6cfeba380f23835136a7deac66e595865
Natalie Halawani <hala1224@gmail.com>
2019-02-17 04:02:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'Male', 'Brown', 'Happy', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'Male', 'Brown', 'Rowdy', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'Brown', 'Happy', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'Male', 'Brown', 'Happy', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'Female', 'Black', 'Sad', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'Male', 'Brown', 'Grumpy', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 4, 'Female', 'Black', 'Shy', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NIL, 6,  'Male', 'Black', 'Aggresive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 2, 'Male', 'Brown', 'Happy', 0),
+('Rowdy', 3, 'Male', 'Brown', 'Rowdy', 1),
+('Tabitha', 2, 'Female', 'Brown', 'Happy', 1),
+('Sergeant Brown', 5, 'Male', 'Brown', 'Happy', 0),
+('Melissa', 5, 'Female', 'Black', 'Sad', 1),
+('Grinch', 7, 'Male', 'Brown', 'Grumpy', 0),
+('Wendy', 4, 'Female', 'Black', 'Shy', 1),
+(NIL, 6,  'Male', 'Black', 'Aggresive', 1);
